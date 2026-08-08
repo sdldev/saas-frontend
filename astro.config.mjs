dfb6673fb0133b8adc108b0baacdd4c0c8bbcdee
root <root@dev.dev>
2026-08-08 03:17:27 +0000

    chore: scaffold Astro SSR project

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
+
+export default defineConfig({
+  output: 'server',
+  adapter: node({ mode: 'standalone' }),
+  server: { port: 4321, host: true },
+});
